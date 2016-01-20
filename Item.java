99a9726f7b17bb6a2d290a9e610a7f3ca53f59a0
jdong1 <JDong1@Stuy.edu>
2016-01-20 05:37:14 +0000

    Item interface

---
+++ Item.java
@@ -0,0 +1,10 @@
+interface Item {
+    
+    /*two types of items:
+      - edibles: provide temporary bonuses, meant for use during combat
+      - wearables: provide "permanent" bonuses, as long as the item is equipped
+    */
+    
+    public void use();
+    
+}
