11966bcb0bf388cbde2269a160c81e310efd8ac9
yabaschu <stanleyzeng1998@gmail.com>
2016-01-20 05:17:17 -0500

    Updating Archer.java

--- Archer.java
+++ Archer.java
@@ -6,30 +6,33 @@ public Archer extends Playable{
         
     }
     
-        
-    
-  public int BlindingArrow(Character target){
+  public int BlindingArrow(Character targets){
       //lowers the monsters accuracy
-      int a = target.getAccuracy();
-      target.setAccuracy(a -= 15);
-      int damage =  (1  * ((Archer.getStrength() * 10) - (target.getResistance() * 7)))
+      mana -= 10;
+      targets.setAccuracy((targets.getAccuracy - 20));
+      int damage = (int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20));
       return damage;
       
   }
   
   public int Volley(){
       //fires 3 shots at enemies
-      int damage = (3 * ((Archer.getStrength()* 10) - (target.getResistance() * 7)));
+      mana -= 10;
+      int damage = ((int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20)))*3;
       return damage;
+      
   }
   
   public void Focus(){
       //increases accuracy and damage
   }
-  
-  public int SlowingShot(){
+
+  public int SlowingShot(Character targets){
       //lowers priority will implement later
-      
+      mana -= 10;
+      int damage = ((int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20)));
+      return damage;
+              
   }
     
     
