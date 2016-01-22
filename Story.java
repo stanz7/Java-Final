c0fe7c36c8b8337d005f5818a7f787f34fd7ab93
LGummy <leith@conybeare.net>
2016-01-22 00:17:02 +0000

    Story skeleton file

---
+++ Story.java
@@ -0,0 +1,29 @@
+public class Story {
+    
+    public Team party = new Team();
+    public int morality;
+    
+    //displays the introductory story and prompts team creation
+    //returns the playable team
+    public static Team prelude() {
+        //things
+    }
+    
+    //runs the first story block
+    public static void Ch1() {
+        //more things
+    }
+    
+    public static void Ch2() {
+        
+    }
+    
+    public static void Ch3() {
+        
+    }
+    
+    public static void Finale() {
+        
+    }
+    
+}
