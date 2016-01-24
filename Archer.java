931821fac78563bacb7a003e245c782f9edaa814
yabaschu <stanleyzeng1998@gmail.com>
2016-01-24 17:04:19 -0500

    99%

--- Archer.java
+++ Archer.java
@@ -1,16 +1,15 @@
-public Archer extends Playable{
+public class Archer extends Playable{
    
-   public Archer(){ //default constructor
-        Rep = 0;
-        EXP = 0;
-        
-    }
+   public Archer(String name){ //default constructor
+      setName(name);
+      setHealth(100);
+   }
     
-  public int BlindingArrow(Character targets){
+   public int BlindingArrow(Combatant targets){
       //lowers the monsters accuracy
       mana -= 10;
-      targets.setAccuracy((targets.getAccuracy - 20));
-      int damage = (int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20));
+      targets.setAccuracy((targets.getAccuracy() - 20));
+      int damage = (int)(Math.random() * (getStrength()) - (getStrength() - 20)) +(getStrength() - 20);
       return damage;
       
   }
@@ -18,19 +17,19 @@ public Archer extends Playable{
   public int Volley(){
       //fires 3 shots at enemies
       mana -= 10;
-      int damage = ((int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20)))*3;
+      int damage = ((int)(Math.random() * (getStrength() - (getStrength() - 20)) +(getStrength() - 20)))*3;
       return damage;
       
   }
   
-  public void Focus(){
+  //public void Focus(){
       //increases accuracy and damage
-  }
+  //}
 
   public int SlowingShot(Character targets){
       //lowers priority will implement later
       mana -= 10;
-      int damage = ((int)(Math.random() * ((Archer.getStrength()) - (Archer.getStrength() - 20)) +(Archer.getStrength() - 20)));
+      int damage = ((int)(Math.random() * (getStrength() - (getStrength() - 20)) +(getStrength() - 20)));
       return damage;
               
   }
