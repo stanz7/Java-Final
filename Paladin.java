690a519a2ffbf28dd520aeeb7dc11a20f1460afa
LGummy <leith@conybeare.net>
2016-01-25 00:11:58 +0000

    righteousHammer ability now works

--- Paladin.java
+++ Paladin.java
@@ -1,25 +1,18 @@
 public class Paladin extends Playable {
     
-    //Something in this file is causing an exception throw*
+    protected Story story;
     
     public Paladin(String name) {
         setName(name);
         setHealth( 100 );
-        
-        /*
-        setHealthMax( 100 );
-        setManaMax( 100 );
-        setMana( 100 );
-        setStrength( 50 );
-        setResistance( 50 );
-        setIntelligence( 80 );
-        setWisdom( 80 );
-        setAccuracy( 75 );
-        setSpeed( 10 );
-        setExp( 0 );
-        setLevel( 0 );
-        */
     }
+    
+    public Paladin( String name, Story input ) {
+        setName( name );
+        setHealth( 100 );
+        story = input;
+    }
+    
     //deals damage
     public int strike() {
         int damage = (int)(Math.random() * getStrength() - (getStrength() - 20) + (getStrength() - 20));
@@ -43,17 +36,18 @@ public class Paladin extends Playable {
         teammate2.setResistance( teammate2.getResistance() + 10 );
     }
     
-    /*CANT DO THIS WITHOUT BEING ABLE TO REFER TO THE CURRENT STORY
     //deals weak damage and strengthens party
     public int righteousHammer( Combatant target) {
         mana -= 10;
-        currentStory.getMember(0).setStrength( currentStory.getMember(0).getStrength() + 10 );
-        Team.getMember(1).setStrength( Team.getMember(1).getStrength() + 10 );
-        setStrength( getStrength() + 10 );
+        Combatant mem1 = new Combatant();
+        mem1 = story.getParty().getMember(0);
+        Combatant mem2 = new Combatant();
+        mem2 = story.getParty().getMember(1);
+        mem1.setStrength( mem1.getStrength() + 10 );
+        mem2.setStrength( mem2.getStrength() + 10 );
         int damage = (int)(Math.random() * getStrength() - (getStrength() - 20) + (getStrength() - 20));
         return damage;
     }
-    */
     
     
 }
