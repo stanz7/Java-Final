3171d32b1d36d013dffc603441904b18238baa9b
yabaschu <stanleyzeng1998@gmail.com>
2016-01-25 07:44:01 -0500

    Create Creation.java

---
+++ Creation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Creation extends Mode {
+    
+    protected static Scanner user;
+    protected static String answer;
+
+    public Creation () {
+    user = new Scanner(System.in);
+    answer = "";
+    }
+    
+    public static void prelude () {
+        System.out.println("");
+
+    }
+}
