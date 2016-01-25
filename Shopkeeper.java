037e2f56f3bd7082032cbc1f6a03a226766afef9
yabaschu <stanleyzeng1998@gmail.com>
2016-01-25 07:48:36 -0500

    Create Shopkeeper.java

---
+++ Shopkeeper.java
@@ -0,0 +1,10 @@
+class Shopkeeper extends Character {
+    
+    //what weapons the shop sells
+    public Item weaponsSold;
+    //what armor that shop sells
+    public Item armorSold;
+    //what potions the shop sells
+    public Item potionsSold;
+    
+}
